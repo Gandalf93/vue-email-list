cd1c9bcffdf51fe92ca5773338f4994b00194449
Giulia <giuliatullia93@gmail.com>
2021-07-04 23:06:50 +0200

    add-mail-random

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+const app = new Vue(
+    {
+        el : '#app',
+        data : {
+            email : '0',
+        },
+        mounted(){
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then( result => {
+                    this.email = result.data.response;
+                    console.log(result.data);
+                })
+                .catch(error =>{
+                    console.log(error);
+                });
+        }
+    }
+);
